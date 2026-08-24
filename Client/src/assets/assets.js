dd33c1f5133dd2f631e29b3c3e3b3fdfffd679ef
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-24 14:32:27 +0530

    Add dummy dashboard data

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -10,3 +10,26 @@ export const dummyProfileData = {
   email: "sathyapriya@example.com",
   image: null,
 };
+
+export const dummyEmployeeDashboardData = {
+  currentMonthAttendance: 20,
+  pendingLeaves: 2,
+  latestPayslip: {
+    netSalary: 2000,
+  },
+
+  employee: {
+    firstName: "Sathya",
+    lastName: "Priya",
+    position: "Software Engineer",
+    department: "Engineering",
+  },
+};
+
+export const dummyAdminDashboardData = {
+  role: "ADMIN",
+  totalEmployees: 3,
+  totalDepartments: 10,
+  todayAttendance: 1,
+  pendingLeaves: 1,
+};
