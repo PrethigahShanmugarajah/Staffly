cbe660b475cbdcfe7536e8917f3ec6cea5a05314
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-24 12:29:31 +0530

    Add dummy profile data

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -2,3 +2,11 @@
 import Logo from "./Logo.png";
 
 export { Logo };
+
+export const dummyProfileData = {
+  _id: "69b411e6f8a807df391d7b13",
+  firstName: "Sathya",
+  lastName: "Priya",
+  email: "sathyapriya@example.com",
+  image: null,
+};
