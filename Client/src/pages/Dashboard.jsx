66690665eafb3cfae0efc0e9050394ce884628ba
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-24 14:34:06 +0530

    Add dashboard data and role based views

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,11 +1,36 @@
 // Client / src / pages / Dashboard.jsx
+import { useEffect, useState } from "react";
+import { dummyEmployeeDashboardData } from "../assets/assets";
+import Loading from "../components/Loading";
+import AdminDashboard from "../components/Dashboard/AdminDashboard";
+import EmployeeDashboard from "../components/Dashboard/EmployeeDashboard";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setData(dummyEmployeeDashboardData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) return <Loading size="xl" />;
+
+  if (!data)
+    return (
+      <p className="text-center text-slate-500 py-12">
+        Failed to load dashboard
+      </p>
+    );
+
+  if (data.role === "ADMIN") {
+    return <AdminDashboard data={data} />;
+  } else {
+    return <EmployeeDashboard data={data} />;
+  }
 };
 
 export default Dashboard;
