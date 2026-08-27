8a0aab40bde16db66c014bc08bd072dd3bada5ff
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 00:16:35 +0530

    Add print payslip route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,6 +10,7 @@ import Attendance from "./pages/Attendance";
 import Leave from "./pages/Leave";
 import Payslips from "./pages/Payslips";
 import Settings from "./pages/Settings";
+import PrintPayslip from "./pages/PrintPayslip";
 
 const App = () => {
   return (
@@ -46,7 +47,7 @@ const App = () => {
           <Route path="/payslips" element={<Payslips />} />
           <Route path="/settings" element={<Settings />} />
         </Route>
-
+        <Route path="/print/payslips/:id" element={<PrintPayslip />} />
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </>
