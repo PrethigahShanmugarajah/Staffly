8c1d95f2eb9f380e33983b931869c24f5a8c92d3
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 00:35:06 +0530

    Add initial Staffly server files

---
+++ Server/server.js
@@ -0,0 +1,23 @@
+// Server / server.js
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import multer from "multer";
+
+/* -------- INITIALIZE EXPRESS -------- */
+const app = express();
+
+/* -------- MIDDLEWARE CONFIGURATION -------- */
+app.use(cors());
+app.use(express.json());
+app.use(multer().none());
+
+/* -------- ROUTES -------- */
+app.get("/", (req, res) => res.send("API is Working!"));
+
+/* -------- PORT -------- */
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
