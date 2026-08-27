ea5f6e9fed090b6e344add9c66773769466228e3
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-27 15:34:49 +0530

    Add dummy attendance data

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -123,3 +123,30 @@ export const dummyEmployeeData = [
     },
   },
 ];
+
+export const dummyAttendanceData = [
+  {
+    _id: "69b68d19f4437fdd254d5a68",
+    employeeId: "69b411e6f8a807df391d7b13",
+    date: "2026-03-14T18:30:00.000Z",
+    checkIn: "2026-03-15T10:42:33.966Z",
+    checkOut: "2026-03-15T18:42:37.476Z",
+    status: "PRESENT",
+    workingHours: 8,
+    dayType: "Full Day",
+    createdAt: "2026-03-15T10:42:33.973Z",
+    updatedAt: "2026-03-15T10:42:37.479Z",
+  },
+  {
+    _id: "69b415b9f8a807df391d7bcc",
+    employeeId: "69b411e6f8a807df391d7b13",
+    date: "2026-03-12T18:30:00.000Z",
+    checkIn: "2026-03-13T13:48:41.416Z",
+    checkOut: "2026-03-13T21:48:42.430Z",
+    status: "PRESENT",
+    workingHours: 8,
+    dayType: "Full Day",
+    createdAt: "2026-03-13T13:48:41.418Z",
+    updatedAt: "2026-03-13T13:48:42.433Z",
+  },
+];
