423ea9c5fc0535a7c5ed6854848af88f3e8f7e48
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 22:09:15 +0530

    Add get attendance route

--- Server/routes/attendanceRoutes.js
+++ Server/routes/attendanceRoutes.js
@@ -1,10 +1,14 @@
 // Server / routes / attendanceRoutes.js
 import { Router } from "express";
-import { clockInOut } from "../controllers/attendanceController.js";
+import {
+  clockInOut,
+  getAttendance,
+} from "../controllers/attendanceController.js";
 import { protect } from "../middleware/auth.js";
 
 const attendanceRouter = Router();
 
 attendanceRouter.post("/", protect, clockInOut);
+attendanceRouter.get("/", protect, getAttendance);
 
 export default attendanceRouter;
