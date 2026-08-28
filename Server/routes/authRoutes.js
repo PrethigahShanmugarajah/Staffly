fe7d2ba3f8b527ceb5224293edf108b504f29485
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 18:43:28 +0530

    Add login route

---
+++ Server/routes/authRoutes.js
@@ -0,0 +1,9 @@
+// Server / routes / authRoutes.js
+import { Router } from "express";
+import { login } from "../controllers/authController.js";
+
+const authRouter = Router();
+
+authRouter.post("/login", login);
+
+export default authRouter;
