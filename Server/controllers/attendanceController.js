d4e0d1d90c426db62e3a032af94edce9711972f7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 22:08:38 +0530

    Add get employee attendance controller

--- Server/controllers/attendanceController.js
+++ Server/controllers/attendanceController.js
@@ -101,3 +101,42 @@ export const clockInOut = async (req, res) => {
     });
   }
 };
+
+/* -------- Get Attendance for Employee -------- */
+export const getAttendance = async (req, res) => {
+  try {
+    const session = req.session;
+    const employee = await Employee.findOne({ userId: session.userId });
+
+    if (!employee) {
+      return res.status(404).json({
+        success: false,
+        message: "Employee not found.",
+      });
+    }
+
+    const limit = parseInt(req.query.limit || 30);
+    const history = await Attendance.find({ employeeId: employee._id })
+      .sort({ date: -1 })
+      .limit(limit);
+
+    return res.status(200).json({
+      success: true,
+      message: "Attendance records retrieved successfully.",
+      data: history,
+      employee: { isDeleted: employee.isDeleted },
+    });
+  } catch (error) {
+    console.error(
+      "Get Attendance for Employee Error:",
+      error?.stack || error?.message || error,
+    );
+
+    return res.status(500).json({
+      success: false,
+      message:
+        "An unexpected error occurred while retrieving attendance records.",
+      error: `Get Attendance for Employee Error: ${error?.stack || error?.message || error}`,
+    });
+  }
+};
