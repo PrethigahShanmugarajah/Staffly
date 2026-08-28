6d67796138f5e907ec3d0fc869b0c79704897184
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 22:41:04 +0530

    Add update leave status route

--- Server/routes/leaveApplicationRoutes.js
+++ Server/routes/leaveApplicationRoutes.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 import {
   createLeave,
   getLeaves,
+  updateLeaveStatus,
 } from "../controllers/leaveApplicationController.js";
-import { protect } from "../middleware/auth.js";
+import { protect, protectAdmin } from "../middleware/auth.js";
 
 const leaveApplicationRouter = Router();
 
 leaveApplicationRouter.post("/", protect, createLeave);
 leaveApplicationRouter.get("/", protect, getLeaves);
+leaveApplicationRouter.patch("/:id", protect, protectAdmin, updateLeaveStatus);
 
 export default leaveApplicationRouter;
