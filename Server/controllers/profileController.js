9a87ef206cdf41460cf0f7ec685e6816b995556d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 21:43:45 +0530

    Add update profile controller

--- Server/controllers/profileController.js
+++ Server/controllers/profileController.js
@@ -35,3 +35,51 @@ export const getProfile = async (req, res) => {
     });
   }
 };
+
+/* -------- Update Profile -------- */
+export const updateProfile = async (req, res) => {
+  try {
+    const session = req.session;
+
+    const employee = await Employee.findOne({ userId: session.userId });
+
+    if (!employee) {
+      return res.status(404).json({
+        success: false,
+        message: "Employee profile not found.",
+      });
+    }
+
+    if (employee.isDeleted) {
+      return res.status(403).json({
+        success: false,
+        message: "Your account is deactivated. You cannot update your profile.",
+      });
+    }
+
+    // await Employee.findByIdAndUpdate(employee._id, { bio: req.body.bio });
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+      employee._id,
+      { bio: req.body.bio },
+      { new: true },
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Profile updated successfully.",
+      // employee,
+      employee: updatedEmployee,
+    });
+  } catch (error) {
+    console.error(
+      "Update Profile Error:",
+      error?.stack || error?.message || error,
+    );
+
+    return res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while updating the profile.",
+      error: `Update Profile Error: ${error?.stack || error?.message || error}`,
+    });
+  }
+};
