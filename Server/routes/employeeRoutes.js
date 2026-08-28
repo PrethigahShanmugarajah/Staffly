325d511ee6695222c3dec20a9a0d91ab6973301e
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 20:45:53 +0530

    Add employee creation route

--- Server/routes/employeeRoutes.js
+++ Server/routes/employeeRoutes.js
@@ -1,10 +1,12 @@
 // Server / routes / employeeRoutes.js
 import { Router } from "express";
-import { getEmployees } from "../controllers/employeeController.js";
+import { createEmployee, getEmployees } from "../controllers/employeeController.js";
 import { protect, protectAdmin } from "../middleware/auth.js";
 
 const employeesRouter = Router();
 
 employeesRouter.get("/", protect, protectAdmin, getEmployees);
+employeesRouter.post("/", protect, protectAdmin, createEmployee);
+
 
 export default employeesRouter;
