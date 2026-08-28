4bc1e47ee8201dcd34463635b6b91c044ccb6901
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 19:45:58 +0530

    Add protected session route

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,9 +1,11 @@
 // Server / routes / authRoutes.js
 import { Router } from "express";
-import { login } from "../controllers/authController.js";
+import { login, session } from "../controllers/authController.js";
+import { protect } from "../middleware/auth.js";
 
 const authRouter = Router();
 
 authRouter.post("/login", login);
+authRouter.get("/session", protect, session);
 
 export default authRouter;
