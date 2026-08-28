792d94cbd69864caba4320dc29818df61c75894d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-28 22:29:58 +0530

    Add get leaves route

--- Server/routes/leaveApplicationRoutes.js
+++ Server/routes/leaveApplicationRoutes.js
@@ -1,10 +1,14 @@
 // Server / routes / leaveApplicationRoutes.js
 import { Router } from "express";
-import { createLeave } from "../controllers/leaveApplicationController.js";
+import {
+  createLeave,
+  getLeaves,
+} from "../controllers/leaveApplicationController.js";
 import { protect } from "../middleware/auth.js";
 
 const leaveApplicationRouter = Router();
 
 leaveApplicationRouter.post("/", protect, createLeave);
+leaveApplicationRouter.get("/", protect, getLeaves);
 
 export default leaveApplicationRouter;
