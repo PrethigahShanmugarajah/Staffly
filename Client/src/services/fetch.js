b569fe63c0c6400e5ac8b041960bb1e1a71cb255
PrethigahShanmugarajah <prethigah@gmail.com>
2026-08-30 20:01:53 +0530

    Add profile fetching and simplify session fetching

--- Client/src/services/fetch.js
+++ Client/src/services/fetch.js
@@ -4,11 +4,9 @@ import API_ROUTES from "../api/api_route";
 import api from "../api/axios";
 
 /* -------- Fetch Session  -------- */
-export const fetchSession = async (token) => {
+export const fetchSession = async () => {
   try {
-    const { data } = await api.get(API_ROUTES.AUTH.SESSION, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const { data } = await api.get(API_ROUTES.AUTH.SESSION);
 
     console.log("Fetch Session API Response:", data);
 
@@ -27,6 +25,33 @@ export const fetchSession = async (token) => {
   } catch (error) {
     toast.error(error?.response?.data?.message || error?.message);
     console.error("Fetch Session Error:", error);
+
+    throw error;
+  }
+};
+
+/* -------- Fetch Profile  -------- */
+export const fetchProfile = async () => {
+  try {
+    const { data } = await api.get(API_ROUTES.PROFILE.SESSION);
+
+    console.log("Fetch Profile API Response:", data);
+
+    if (data?.success) {
+      // toast.success(data?.message);
+      console.log("Fetch Profile Success:", data?.message);
+    } else {
+      toast(data?.message || "Fetch profile with warning");
+      console.warn(
+        "Fetch Profile Warning:",
+        data?.message || "Fetch Profile Warning",
+      );
+    }
+
+    return data;
+  } catch (error) {
+    toast.error(error?.response?.data?.message || error?.message);
+    console.error("Fetch Profile Error:", error);
 
     throw error;
   }
